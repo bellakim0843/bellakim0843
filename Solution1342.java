391eb619c87a384b849cee3525cd0ae99fdffb50
bellakim0843 <bellakim0843@gmail.com>
2022-11-28 21:26:43 +0000

    second commit

---
+++ Solution1342.java
@@ -0,0 +1,17 @@
+class Solution1342 {
+    public int numberOfSteps(int num) {
+        int count = 0;
+
+        while(num != 0){
+            if(num%2 == 0){
+                num = num/2;
+            }else
+                num--;
+                count++;
+
+
+        }
+       return count;
+    }
+
+}
